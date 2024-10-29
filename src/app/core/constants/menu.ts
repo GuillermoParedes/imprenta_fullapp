9f44e8592b61fdf85f0a81e1bed47ee246d52f92
Codevs <guillermo.paredes@tutatordev.com>
2024-10-29 07:53:43 -0400

    imp: add contadores en dashboard

--- src/app/core/constants/menu.ts
+++ src/app/core/constants/menu.ts
@@ -10,7 +10,7 @@ export class Menu {
           icon: 'assets/icons/heroicons/outline/chart-pie.svg',
           label: 'Dashboard',
           route: '/home/dashboard',
-         
+
         },
         {
           icon: 'assets/icons/heroicons/outline/cube.svg',
@@ -27,37 +27,10 @@ export class Menu {
           label: 'Pedidos',
           route: '/home/orders',
         },
-        {
-          icon: 'assets/icons/heroicons/outline/cube.svg',
-          label: 'Reportes',
-          route: '/home/dashboard',
-        },
-        // {
-        //   icon: 'assets/icons/heroicons/outline/lock-closed.svg',
-        //   label: 'Auth',
-        //   route: '/auth',
-        //   children: [
-        //     { label: 'Sign up', route: '/auth/sign-up' },
-        //     { label: 'Sign in', route: '/auth/sign-in' },
-        //     { label: 'Forgot Password', route: '/auth/forgot-password' },
-        //     { label: 'New Password', route: '/auth/new-password' },
-        //     { label: 'Two Steps', route: '/auth/two-steps' },
-        //   ],
-        // },
-        // {
-        //   icon: 'assets/icons/heroicons/outline/exclamation-triangle.svg',
-        //   label: 'Erros',
-        //   route: '/home/errors',
-        //   children: [
-        //     { label: '404', route: '/home/errors/404' },
-        //     { label: '500', route: '/home/errors/500' },
-        //   ],
-        // },
         // {
         //   icon: 'assets/icons/heroicons/outline/cube.svg',
-        //   label: 'Components',
-        //   route: '/home/components',
-        //   children: [{ label: 'Table', route: '/home/components/table' }],
+        //   label: 'Reportes',
+        //   route: '/home/dashboard',
         // },
       ],
     },
--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClientsService } from '../clients/clients.service';
+import { ProductsService } from '../products/products.service';
+import { OrdersService } from '../orders/orders.service';
 
 @Component({
-    selector: 'app-dashboard',
-    templateUrl: './dashboard.component.html',
-    standalone: true,
-    imports: [RouterOutlet],
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  standalone: true,
+  imports: [RouterOutlet],
 })
 export class DashboardComponent implements OnInit {
-  constructor() {}
+  clientesCount = 90;
+  productosCount = 193;
+  pedidosCount = 15;
+  constructor(private readonly clientService: ClientsService, private readonly productsService: ProductsService, private readonly pedidosService: OrdersService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+    this.clientService.getClients().then((response: any) => {
+      this.clientesCount = response.length
+    })
+    this.productsService.getProducts().then((response: any) => {
+      this.productosCount = response.length
+    })
+    this.pedidosService.getPedidos().then((response: any) => {
+      this.pedidosCount = response.length
+    })
+
+  }
 }
