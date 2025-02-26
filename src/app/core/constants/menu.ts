14fcf9b716880306a1cc0bd623b48b4f2ef312db
codevs <guillermo.paredes@tutatordev.com>
2025-02-25 20:19:37 -0400

    imp: add change menu sidebar

--- src/app/core/constants/menu.ts
+++ src/app/core/constants/menu.ts
@@ -3,17 +3,17 @@ import { MenuItem } from '../models/menu.model';
 export class Menu {
   public static pages: MenuItem[] = [
     {
-      group: 'Base',
+      group: 'MENU',
       separator: false,
       items: [
         {
           icon: 'assets/icons/heroicons/outline/chart-pie.svg',
-          label: 'Dashboard',
+          label: 'Pabel administrativo',
           route: '/home/dashboard',
 
         },
         {
-          icon: 'assets/icons/heroicons/outline/cube.svg',
+          icon: 'assets/icons/heroicons/outline/users.svg',
           label: 'Clientes',
           route: '/home/clients',
         },
@@ -23,15 +23,15 @@ export class Menu {
           route: '/home/products',
         },
         {
-          icon: 'assets/icons/heroicons/outline/cube.svg',
+          icon: 'assets/icons/heroicons/outline/bookmark.svg',
           label: 'Pedidos',
           route: '/home/orders',
         },
-        // {
-        //   icon: 'assets/icons/heroicons/outline/cube.svg',
-        //   label: 'Reportes',
-        //   route: '/home/dashboard',
-        // },
+        {
+          icon: 'assets/icons/heroicons/outline/information-circle.svg',
+          label: 'Reportes',
+          route: '/home/dashboard',
+        },
       ],
     },
   ];
